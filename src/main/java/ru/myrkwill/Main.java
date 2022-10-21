53a741b47b03e9a2fd283764fc7695ad8e48e074
Mark Nagibin <myrkwill@icloud.com>
2022-10-21 16:50:04 +0500

    Initial

--- src/main/java/ru/myrkwill/Main.java
+++ src/main/java/ru/myrkwill/Main.java
@@ -1,6 +1,9 @@
-package ru.myrkwill;/**
+package ru.myrkwill;
+
+/**
  * @author Mark Nagibin
- */public class Main {
+ */
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
