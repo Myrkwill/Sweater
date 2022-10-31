d76a3d9a78eb424611f10a59186b204a3960bdfc
Mark Nagibin <myrkwill@icloud.com>
2022-10-31 20:28:32 +0500

    Add links between database tables (one to many)

--- src/main/java/ru/myrkwill/sweater/controllers/MessagesController.java
+++ src/main/java/ru/myrkwill/sweater/controllers/MessagesController.java
@@ -1,11 +1,13 @@
 package ru.myrkwill.sweater.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.myrkwill.sweater.entities.Message;
+import ru.myrkwill.sweater.entities.User;
 import ru.myrkwill.sweater.repositories.MessageRepository;
 
 @Controller
@@ -26,12 +28,16 @@ public class MessagesController {
 	}
 
 	@PostMapping("/messages")
-	public String createMessage(@ModelAttribute("message") Message message, BindingResult result, Model model) {
+	public String createMessage(@AuthenticationPrincipal User user,
+								@ModelAttribute("message") Message message,
+								BindingResult result,
+								Model model) {
 		if (result.hasErrors()) {
 			return "messages";
 		}
 
-		messageRepository.save(message);
+		Message newMessage = new Message(message.getText(), message.getTag(), user);
+		messageRepository.save(newMessage);
 		model.addAttribute("message", new Message());
 		model.addAttribute("messages", messageRepository.findAll());
 
--- src/main/java/ru/myrkwill/sweater/entities/Role.java
+++ src/main/java/ru/myrkwill/sweater/entities/Role.java
@@ -1,8 +1,15 @@
 package ru.myrkwill.sweater.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * @author Mark Nagibin
  */
-public enum Role {
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
